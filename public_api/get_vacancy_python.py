b410f4c4def989cae1c0a95fd49bc5fc0fa74805
Alex <lenkoran26@outlook.com>
2023-04-15 21:01:21 +0300

    add handlers photo, stickers

--- public_api/get_vacancy_python.py
+++ public_api/get_vacancy_python.py
@@ -2,38 +2,42 @@ import requests
 from random import randint, sample
 
 def get_random_vacancy():
+    # определяем URL с вакансиями
     URL: str = 'https://api.hh.ru/vacancies?'
 
+    # создаем заголовки, в которых нашем агентом будет не браузер, а тест апи (так требует hh.ru)
     headers: dict = {
         'User-Agent': 'api-test-agent'
     }
-
+    # определяем словарь с параметрами запроса, чтобы не передавать все в строке так:
+    # https://api.hh.ru/vacancies?page=1&per_page=100&text=python&area=2
     params: dict = {
-        'page': '1',
-        'per_page': '100',
-        'text': 'python',
-        'area': '2'
+        'page': '1',        # страница
+        'per_page': '100',  # количество результатов на странице
+        'text': 'python',   # встречается в тексте
+        'area': '2'         # область Санкт-Петербург
     }
 
-    ids: list = [str]
+    ids: list = [str]   #список для id вакансий
     resp = requests.get(URL, headers=headers, params=params)
     data = resp.json()
-    count_page = data['pages']
-    count_id = data['found']
-    # print(resp.status_code)
+    count_page = data['pages']  # количество страниц с результатами
+    count_id = data['found']    # всего вакансий найдено
 
+    # проходимся по каждой странице
     for i in range(count_page + 1):
         resp = requests.get(f'https://api.hh.ru/vacancies?page={i}&per_page=100&text=python&area=2')
         data = resp.json()
+        # и получаем с нее все id вакансий
         for item in data['items']:
             ids.append(item['id'])
     print(f'found ids = {count_id}, parsed ids = {len(ids)}')
-    # print(len(set(ids)))
 
-    rand_id = ids[randint(0, len(ids) - 1)]
+    # получаем три случайных значения из списка id вакансий
     rand_ids = sample(ids, 3)
     list_of_data_vacancy: list = [dict]
 
+    # заходим на страничку каждой вакансии и заносим нужные нам поля в словарь
     for id in rand_ids:
         url_rand_vacancies = f'https://api.hh.ru/vacancies/{id}'
         resp_vacancies = requests.get(url=url_rand_vacancies, headers=headers).json()
@@ -42,6 +46,8 @@ def get_random_vacancy():
                         'salary': resp_vacancies["salary"],
                         'url': resp_vacancies["alternate_url"]
                         }
+        # каждый из словарей заносим в список
+        # заносим
         list_of_data_vacancy.append(data_vacancy.copy())
 
 
--- tg_bot/aio_echo_bot.py
+++ tg_bot/aio_echo_bot.py
@@ -24,13 +24,38 @@ async def process_help_command(message: Message):
 async def send_photo_echo(message: Message):
     await message.reply_photo(message.photo[0].file_id)
 
+# Этот хэндлер будет срабатывать на отправку боту стикера
 @dp.message(F.sticker)
 async def send_sticker_echo(message: Message):
     await message.reply_sticker(message.sticker.file_id)
 
+# Этот хэндлер будет срабатывать на отправку боту любого сообщения
 @dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+
+
+# Регистрируем хэндлеры
+# dp.message.register(process_start_command, Command(commands=["start"]))
+# dp.message.register(process_help_command, Command(commands=['help']))
+# dp.message.register(send_photo_echo, F.photo)
+# dp.message.register(send_sticker_echo, F.sticker)
+# dp.message.register(send_echo)
+
+
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help"
+# @dp.message()
+# async def send_echo(message: Message):
+#     try:
+#         await message.send_copy(chat_id=message.chat.id)
+#     except TypeError:
+#         await message.reply(text='Данный тип апдейтов не поддерживается '
+#                                  'методом send_copy')
+
+
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
