efc9d3473090904cea54bf0eebce256ba7035065
Alex <lenkoran26@outlook.com>
2023-04-17 21:47:47 +0300

    add main.py and import modules

---
+++ public_api/main.py
@@ -0,0 +1,9 @@
+from get_course import get_course
+from get_weather import get_weather_spb
+from get_vacancy_python import get_random_vacancy
+
+
+if __name__ == '__main__':
+    course = get_course()
+    weather = get_weather_spb()
+    vacancies = get_random_vacancy()
